7976808e10f7c9e23d93f6bbf7df27e43ff725a3
Yash86400-Design <knowledge86400@gmail.com>
2024-01-25 10:38:50 +0530

    Remove some extra redundant code and added a background css to username in the header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -39,7 +39,7 @@ function Dashboard() {
   return (
     <>
       <section className="heading">
-        <h1>Welcome {user && user.name}</h1>
+        <h1>Welcome <span className="username">{user && user.name}</span></h1>
         <p>Notes Dashboard</p>
       </section>
       <NoteForm />
